33f3a4eb4bcb2638cb7e250921a7256d90b4d6e9
AdobyY <yarinko.b@gmail.com>
2023-10-21 10:37:26 +0300

    Last commit

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 function getMessages(req, res){
-    res.send('<ul><li>Hello Boh!</li></ul>');
+    res.render('messages',{
+        title: 'Messages to friends',
+        friend: 'Elon',
+    })
+   // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'photo.png')); 
 }
 
 function postMessage(req, res){
